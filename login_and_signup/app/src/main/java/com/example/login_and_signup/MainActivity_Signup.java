3db1ee809e56190165beafb6c6453363903617e8
QuocVuong14 <71715309+QuocVuong14@users.noreply.github.com>
2020-09-22 22:25:54 +0700

    add layout

---
+++ login_and_signup/app/src/main/java/com/example/login_and_signup/MainActivity_Signup.java
@@ -0,0 +1,14 @@
+package com.example.login_and_signup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity_Signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main__signup);
+    }
+}
